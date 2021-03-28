5445f54ee6c9a1fb4500cb9af34d9f7a40af2717
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-03-28 12:36:41 +0530

    Create Sorting_Comparator_HR.cpp

---
+++ Sorting_Comparator_HR.cpp
@@ -0,0 +1,63 @@
+Link for ques:
+https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=arcesium
+
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
+
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+	if(a.score!=b.score)
+    {
+    if(a.score>b.score)
+       return 1;
+       else  {
+       return -1;
+       }
+    }
+ 
+      if(a.name>b.name)
+            return -1;
+        else {
+        return 1;
+        }
+    }
+};
+
+
+
+
+bool compare(Player a, Player b) {
+    if(Checker::comparator(a,b) == -1)
+        return false;
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player player;
+        player.name = name, player.score = score;
+        players.push_back(player);
+    }
+    sort(players.begin(), players.end(), compare);
+    for(int i = 0; i < players.size(); i++) {
+        cout << players[i].name << " " << players[i].score << endl;
+    }
+    return 0;
+}
