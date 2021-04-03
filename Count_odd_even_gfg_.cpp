d03693bbdc9102a32e2a72b28d25ae96a28e8255
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-04-03 22:54:16 +0530

    Create Count_odd_even_gfg_.cpp

---
+++ Count_odd_even_gfg_.cpp
@@ -0,0 +1,63 @@
+Link For Question:
+
+https://practice.geeksforgeeks.org/problems/count-odd-even/1
+
+
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    /*Function to count even and odd elements in the array
+    * arr : Array with its elements
+    * sizeof_array : number of array elements
+    * countOdd : variable to count number of odd elements
+    * countEven : variable to count number of even elements
+    */
+    void countOddEven(int arr[], int sizeof_array)
+    {
+        
+        // your code here
+       int cnt_e=0;
+        int cnt_o=0;// your code here
+        for(int i=0;i<sizeof_array;i++)
+        {
+            if(arr[i]&1)
+            cnt_o++;
+            else
+            cnt_e++;
+        }
+        cout<<cnt_o<<" "<<cnt_e<<"\n";
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	
+	// Testcase input
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int sizeof_array;
+	    cin >> sizeof_array;
+	    
+	    int arr[sizeof_array];
+	    
+	    // Array elements input
+	    for(int i = 0; i < sizeof_array; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    // Calling function to count even and odd
+	    ob.countOddEven(arr, sizeof_array);
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
