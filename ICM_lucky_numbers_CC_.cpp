a62084350288a1d338e1e706eda800207551dd32
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-04-07 20:19:31 +0530

    Create ICM_lucky_numbers_CC_.cpp

---
+++ ICM_lucky_numbers_CC_.cpp
@@ -0,0 +1,22 @@
+Link for Ques:
+https://www.codechef.com/ICM2021B/problems/ICM0013
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+
+using namespace std;
+int main()
+{    fastio;
+int  t;
+cin>>t;
+while(t--)
+ {
+     int n;
+     cin>>n;
+     cout<<n<<endl;
+ }
+     
+}
