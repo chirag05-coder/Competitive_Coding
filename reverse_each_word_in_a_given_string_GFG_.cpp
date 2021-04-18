898d933c476349fee3c1fd41689026a3c30882ba
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-04-18 12:37:24 +0530

    Create reverse_each_word_in_a_given_string_GFG_.cpp

---
+++ reverse_each_word_in_a_given_string_GFG_.cpp
@@ -0,0 +1,53 @@
+Link For Ques:
+https://practice.geeksforgeeks.org/problems/reverse-each-word-in-a-given-string1001/1#
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+  public:
+    
+    string reverseWords (string s)
+    {   string str;
+       stack<char>st;
+       s.push_back('.');
+       for(int i=0;i<s.length();i++)
+       {      if(s.at(i)=='.')
+              {   while(!st.empty())
+                 {
+                     str.push_back(st.top());
+                     st.pop();
+                 }
+                 str.push_back('.');
+                  
+              }
+              else
+              st.push(s.at(i));
+           
+       }
+       str.pop_back();
+       return str;
+    }
+        
+       
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout << ob.reverseWords (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
