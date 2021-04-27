789b02103ce066c0bec4ebd80a318ccc2bc442cb
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-04-27 18:30:12 +0530

    Create 1335_B_codeforces_construct_the_string_.cpp

---
+++ 1335_B_codeforces_construct_the_string_.cpp
@@ -0,0 +1,37 @@
+Link for QUes:
+https://codeforces.com/problemset/problem/1335/B
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+
+using namespace std;
+
+int main()
+{    fastio;
+    int t;
+   cin>>t;
+   while(t--){
+       int n,a,b;
+       cin>>n>>a>>b;
+       string str;
+       for(int i=97;i<97+b;i++)
+       {
+           str.push_back(char(i));
+       }
+       while(str.size()<n)
+       {
+           str+=str;
+       }
+       while(str.size()!=n)
+       {
+           str.pop_back();
+       }
+       cout<<str<<"\n";
+   }
+    
+   
+    
+}
