50f3f7b4b0bd4eab9bbe4424794ac41a3d71a48b
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-04-28 18:52:11 +0530

    Create 1358_a_park_lighting_codeforces_.cpp

---
+++ 1358_a_park_lighting_codeforces_.cpp
@@ -0,0 +1,35 @@
+Link for Ques:
+https://www.google.com/url?q=https://codeforces.com/problemset/problem/1358/A&sa=D&source=hangouts&ust=1619701741927000&usg=AFQjCNFwzebllQRuY5hs9Kb53z3L0_EvsQ
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+
+using namespace std;
+
+int main()
+{    fastio;
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       long long int n,m;
+       cin>>n>>m;
+       if((n*m)%2==0)
+       {
+           cout<<(m*n)/2<<endl;
+       }
+       else
+       {
+           cout<<((n*m)/2)+1<<endl;
+       }
+   }
+   
+   
+    
+   
+    
+}
