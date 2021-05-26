8b3048f3afd547954dad50c0c77335289b03c13c
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-05-27 00:42:54 +0530

    Leetcode

---
+++ Flipping_an_image_Leetcode_.cpp
@@ -0,0 +1,23 @@
+Link for Ques:
+https://leetcode.com/problems/flipping-an-image/
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        
+        
+        for(int i=0;i<image.size();i++)
+        {
+              reverse(image[i].begin(),image[i].end());
+        }
+        for(int i=0;i<image.size();i++)
+        {
+               for(int j=0;j<image.size();j++)
+               {
+                     image[i][j]=image[i][j]^1;
+               }
+        }
+        return image;
+        
+    }
+};
