1899c1079ec2471d71fb54cdb61bafc6dc1d291c
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-06-29 15:02:55 +0530

    GeeksForGeeks

---
+++ Reverse_a_linked_list_GFG_.cpp
@@ -0,0 +1,113 @@
+Link for Ques:
+https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1#
+
+
+//Initial Template for C++// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+ // } Driver Code Ends
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *&head)
+    {
+        // code here
+        // return head of reversed list
+          struct Node *p,*c,*n;
+          
+          p=NULL;
+          c=head;
+          n=c->next;
+          while(n!=NULL)
+          {
+                
+                c->next=p;
+                p=c;
+                c=n;
+                
+                n=n->next;
+          }
+          c->next=p;
+                p=c;
+                c=n;
+        //   head=c;
+        //   return head;
+        return p;
+    }
+    
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
