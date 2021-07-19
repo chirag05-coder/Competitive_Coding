9328150ee055280af8f643d08fb8628e199d42a9
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-07-20 00:41:25 +0530

    Codeforces

---
+++ Binary decimal.cpp
@@ -0,0 +1,68 @@
+Link for Ques:
+https://codeforces.com/contest/1544/problem/A
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+int t;
+cin>>t;
+while(t--)
+{
+      lli n;
+      cin>>n;
+      
+      lli maxii=0;
+      while(n>0)
+      {
+          maxii=max(n%10,maxii);
+          n=n/10;
+      }
+      cout<<maxii<<endl;
+}
+
+      
+}
+
+
+     
