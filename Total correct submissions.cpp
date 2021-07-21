ea412c94dd710532fd0c08996a8a489ec48dbfbf
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-07-21 14:32:26 +0530

    Codechef Starters June

---
+++ Total correct submissions.cpp
@@ -0,0 +1,84 @@
+Link for Ques:
+https://www.codechef.com/START5C/problems/TOTCRT
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+      int t ;
+      cin>>t;
+      while(t--)
+      {
+            int n;
+            cin>>n;
+            unordered_map<string,int>umap;
+            string str;
+            int x;
+            multiset<int>st;
+            for(int i=0;i<3*n;i++)
+            {
+                  cin>>str;
+                  cin>>x;
+                  if(umap.find(str)!=umap.end())
+                  {
+                        umap[str]=umap[str]+x;
+                  }
+                  else
+                  umap[str]=x;
+            }
+            for(auto it:umap)
+            {
+                st.insert(it.second);
+            }
+            for(auto it:st)
+            {
+                 cout<<it<<" ";
+            }
+            cout<<endl;
+      }
+      
+}
+
+
+     
