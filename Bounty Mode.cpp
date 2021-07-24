1883a86ab2edc014d4df06158719ded7562f1434
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-07-24 21:17:16 +0530

    Codechef

---
+++ Bounty Mode.cpp
@@ -0,0 +1,75 @@
+Link for QUes:
+https://www.codechef.com/CPCD2021/problems/BOUN
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+     int k;
+     cin>>k;
+     int n;
+     cin>>n;
+     int arr[n];
+     unordered_map<int,int>umap;
+     int flag=0;
+     for(int i=0;i<n;i++)
+     {
+          cin>>arr[i];
+          umap[arr[i]]++;
+     }
+     for(int i=0;i<n;i++)
+     {
+           if(umap.find(k-arr[i])!=umap.end())
+             {  flag++;
+                 cout<<"true";
+                 break;
+             }
+     }
+     if(flag==0)
+     cout<<"false";
+
+      
+}
+
+
+     
