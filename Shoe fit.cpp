5faef1652683c7d61c313c92754da584d7e12b17
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-07-30 21:55:03 +0530

    Codechef July cookoff 2021

---
+++ Shoe fit.cpp
@@ -0,0 +1,67 @@
+Link for Ques:
+https://www.codechef.com/COOK131C/problems/SHOEFIT
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+ int t;
+ cin>>t;
+ while(t--)
+ {
+     int a,b,c;
+     cin>>a>>b>>c;
+     if(a+b+c==2 or a+b+c==1)
+     {
+         cout<<"1"<<endl;
+     }
+     else
+     cout<<"0"<<endl;
+ }
+
+      
+}
+
+
+     
