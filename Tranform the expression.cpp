8f0c2af7a15cbcdbc304c0b13505bafed899cb27
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-08-12 22:33:51 +0530

    SPOJ

---
+++ Tranform the expression.cpp
@@ -0,0 +1,84 @@
+Link for Ques:
+https://www.spoj.com/problems/ONP/
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         string str,res;
+         cin>>str;
+         stack<char>st;
+         for(int i=0;i<str.length();i++)
+         {
+              char ch=str.at(i);
+               if(ch>='a' and ch<='z')
+                res.push_back(ch);
+                else
+                {
+                    if(ch==')')
+                     {   while(!st.empty() and st.top()!='(')
+                            {
+                                 res.push_back(st.top());
+                                 st.pop();
+                            }
+                            if(!st.empty())
+                            st.pop();
+                         
+                     }
+                     else
+                     {
+                         st.push(ch);
+                     }
+                }
+         }
+         cout<<res<<endl;
+     }
+
+      
+}
+
+
+     
