60011cd0c4a8edf5dbf9d80685ede276b1df164c
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-08-20 13:37:54 +0530

    Codeforces 1554A

---
+++ Cherry.cpp
@@ -0,0 +1,79 @@
+Link for Ques:
+https://codeforces.com/problemset/problem/1554/A
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+
+
+
+int main()
+{    fastio;
+ int t;
+ cin>>t;
+ while(t--)
+ {
+       int n;
+       cin>>n;
+       int arr[n]{0};
+       priority_queue<lli>pq;
+       for(int i=0;i<n;i++)
+       {
+           cin>>arr[i];
+       }
+       
+       for(int i=0;i<n;i++)
+       {
+           if(i-1>=0)
+           {
+               pq.push(max(arr[i],arr[i-1])*1LL*(min(arr[i],arr[i-1])));
+           }
+           if(i+1<n)
+           {
+                pq.push(max(arr[i],arr[i+1])*1LL*(min(arr[i],arr[i+1])));
+           }
+       }
+       cout<<pq.top()<<endl;
+ }
+
+      
+}
+
+
+     
