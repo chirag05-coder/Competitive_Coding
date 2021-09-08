2102ff710b14789c16fad369e1351f4590137e8a
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-09-08 23:33:29 +0530

    Codeforces

---
+++ balanced substring.cpp
@@ -0,0 +1,123 @@
+https://codeforces.com/contest/1569/problem/A
+
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+lli highestPowerof2(lli n)
+{
+  lli p = (int)log2(n);
+  // return (int)pow(2, p);   power
+  
+     return p;  //number
+}
+bool is_prime(int num)
+{      if(num==1)
+      return false;
+      if(num==2 || num==3)
+	      return true;
+		if(num%2==0 || num%3==0)
+		     return false;
+	 for(int i=5;i*i<=num;i=i+6)
+	   {      if(num%i==0 || num%(i+2)==0)
+	              return false;
+		   }
+		return true;       
+}
+
+
+int main()
+{    fastio;
+
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+          int n;
+          cin>>n;
+          string str;
+          cin>>str;
+          int a=0,b=0;
+          
+          for(int i=0;i<str.length();i++)
+          {
+                 char ch=str.at(i);
+                   if(ch=='a')
+                    a++;
+                    
+                    else 
+                    b++;
+          }
+           if(a==b)
+           {
+                 cout<<1<<" "<<str.length()<<endl;
+           }
+           else
+           {        int flag=0;
+                        
+                    size_t x=str.find("ab");
+                      if(x!=string::npos)
+                      {
+                            cout<<x+1<<" "<<x+2<<endl;
+                            flag++;
+                      }
+                      
+                        size_t y=str.find("ba");
+                      if(y!=string::npos and flag==0)
+                      {
+                            cout<<y+1<<" "<<y+2<<endl;
+                            flag++;
+                      }
+                      
+                      if(flag==0)
+                      {
+                          cout<<"-1"<<" "<<"-1"<<endl;
+                      }
+                      
+           }
+    }
+
+     
+      
+}
+
+
+     
