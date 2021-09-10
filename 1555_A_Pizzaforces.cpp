d8bdc3b2e138a88660bbbaf7cc8e6b85a171b381
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-09-10 20:13:49 +0530

    Codeforces

---
+++ 1555_A_Pizzaforces.cpp
@@ -0,0 +1,96 @@
+Link for Ques:
+https://codeforces.com/contest/1555/problem/A
+
+
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+lli highestPowerof2(lli n)
+{
+  lli p = (int)log2(n);
+  // return (int)pow(2, p);   power
+  
+     return p;  //number
+}
+bool is_prime(int num)
+{      if(num==1)
+      return false;
+      if(num==2 || num==3)
+	      return true;
+		if(num%2==0 || num%3==0)
+		     return false;
+	 for(int i=5;i*i<=num;i=i+6)
+	   {      if(num%i==0 || num%(i+2)==0)
+	              return false;
+		   }
+		return true;       
+}
+
+
+int main()
+{    fastio;
+      int t;
+     cin>>t;
+    while(t--){
+         unsigned lli n;
+         cin>>n;
+         
+         if(n<=6)
+         cout<<"15\n";
+         else 
+         {
+             if(n&1)
+             {
+                 unsigned lli ans=(n+1)*1LL*(2.5);
+             cout<<ans<<"\n";
+             }
+             else
+             {
+                 unsigned lli ans=(n*1LL*(2.5));
+             cout<<ans<<"\n";
+             }
+         }
+     }
+}
+
+
+
+     
