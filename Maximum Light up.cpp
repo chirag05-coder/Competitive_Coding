6c897148d60e014c356654719b437abb8455c2c0
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-09-26 21:49:17 +0530

    Codechef

---
+++ Maximum Light up.cpp
@@ -0,0 +1,110 @@
+Link for Ques:
+https://www.codechef.com/START13B/problems/DIWALI1
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+lli highestPowerof2(lli n)
+{
+  lli p = (int)log2(n);
+  // return (int)pow(2, p);   power
+  
+     return p;  //number
+}
+bool is_prime(int num)
+{      if(num==1)
+      return false;
+      if(num==2 || num==3)
+	      return true;
+		if(num%2==0 || num%3==0)
+		     return false;
+	 for(int i=5;i*i<=num;i=i+6)
+	   {      if(num%i==0 || num%(i+2)==0)
+	              return false;
+		   }
+		return true;       
+}
+bool isPerfectSquare(long double x)
+{
+    // Find floating point value of
+    // square root of x.
+    if (x >= 0) {
+ 
+        long long sr = sqrt(x);
+         
+        // if product of square root
+        //is equal, then
+        // return T/F
+        return (sr * sr == x);
+    }
+    // else return false if n<0
+    return false;
+}
+
+
+int main()
+{    fastio;
+      int t;
+     cin>>t;
+    while(t--){
+        
+           lli p,a,b,c,x,y;
+           cin>>p>>a>>b>>c>>x>>y;
+           
+           lli ans1=p/((x*1LL*a)+b);
+           lli ans2=p/((y*1LL*a)+c);
+           
+           lli pp1=p;
+           lli pp=p;
+           pp1=p-((x*1LL*a)+b)*1LL*ans1;
+           pp=p-((y*1LL*a)+c)*1LL*ans2;
+           
+           ans1+=pp1/((y*1LL*a)+c);
+           ans2+=pp/((x*1LL*a)+b);
+           
+          
+           
+           cout<<max(ans1,ans2)<<endl;
+           
+     }
+}
+
+
+
+     
