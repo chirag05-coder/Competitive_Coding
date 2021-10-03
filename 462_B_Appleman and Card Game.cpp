5bd3838e0f06991022d6859e4760c1bd24c7721c
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-10-03 11:47:47 +0530

    Codeforces

---
+++ 462_B_Appleman and Card Game.cpp
@@ -0,0 +1,121 @@
+Link for Ques:
+https://codeforces.com/contest/462/problem/B
+
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define lli long long int
+using namespace std;
+const int M=1e9+7;
+lli power(lli a,lli b)
+{
+     if(b==0 )
+        return 1;
+       if(b==1)
+       return a;
+     
+      if(!(b&1)) 
+      return (power(a,b/2)%M)*(power(a,b/2)%M);
+      else
+      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+}
+
+lli  gcd(lli u, lli v)
+{
+   lli shift;
+   if (u == 0) return v;
+   if (v == 0) return u;
+   shift = __builtin_ctz(u | v);
+   u >>= __builtin_ctz(u);
+   do {
+       v >>= __builtin_ctz(v);
+       if (u > v) {
+         lli t = v;
+           v = u;
+           u = t;
+       }  
+       v = v - u;
+   } while (v != 0);
+   return u << shift;
+}
+lli highestPowerof2(lli n)
+{
+  lli p = (int)log2(n);
+  // return (int)pow(2, p);   power
+  
+     return p;  //number
+}
+bool is_prime(int num)
+{      if(num==1)
+      return false;
+      if(num==2 || num==3)
+	      return true;
+		if(num%2==0 || num%3==0)
+		     return false;
+	 for(int i=5;i*i<=num;i=i+6)
+	   {      if(num%i==0 || num%(i+2)==0)
+	              return false;
+		   }
+		return true;       
+}
+bool isPerfectSquare(long double x)
+{
+    // Find floating point value of
+    // square root of x.
+    if (x >= 0) {
+ 
+        long long sr = sqrt(x);
+         
+        // if product of square root
+        //is equal, then
+        // return T/F
+        return (sr * sr == x);
+    }
+    // else return false if n<0
+    return false;
+}
+
+
+int main()
+{    fastio;
+      int n,m;
+      cin>>n>>m;
+      string str;
+      cin>>str;
+      unordered_map<char,int>umap;
+      priority_queue<int>pq;
+      for(auto it:str)
+        umap[it]++;
+        
+        
+        for(auto it:umap)
+          {  pq.push(it.second);
+          
+          }
+          
+            lli ans=0;
+          while(m>0 and !pq.empty())
+          {
+                 if(pq.top()>m)
+                 {
+                         ans=ans+(m*1LL*m);
+                         pq.pop();
+                         m=0;
+                 }
+                 else
+                 {
+                       ans=ans+(pq.top()*1LL*pq.top());
+                        m-=pq.top();
+                       pq.pop();
+                      
+                 }
+          }
+          cout<<ans<<endl;
+}
+
+
+
+     
