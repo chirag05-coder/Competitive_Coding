b4583ff5fa929dbd7340c83d9f0efe04d97b75b1
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-10-16 23:32:09 +0530

    Codeforces

--- 110_A_Nearly Lucky Number.cpp
+++ 110_A_Nearly Lucky Number.cpp
@@ -8,19 +8,7 @@ https://codeforces.com/contest/110/problem/A
 #define lli long long int
 using namespace std;
 const int M=1e9+7;
-lli power(lli a,lli b)
-{
-     if(b==0 )
-        return 1;
-       if(b==1)
-       return a;
-     
-      if(!(b&1)) 
-      return (power(a,b/2)%M)*(power(a,b/2)%M);
-      else
-      return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
 
-}
 
 int main()
 {    fastio;
