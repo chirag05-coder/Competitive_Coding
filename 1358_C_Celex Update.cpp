8ceaa077872886d79ad4fb98db3109757b6987ac
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-10-22 22:07:56 +0530

    Codeforces

---
+++ 1358_C_Celex Update.cpp
@@ -0,0 +1,27 @@
+Link for Ques:
+https://codeforces.com/contest/1358/problem/C
+
+
+#include<bits/stdc++.h>
+#define endl "\n"
+#define int  long long int
+using namespace std;
+
+
+signed main()
+{  
+        int t;
+        cin>>t;
+        while(t--)
+        {
+           int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        cout<<(x2-x1)*1LL*(y2-y1)+1<<endl;
+             
+             
+        }
+}
+
+
+
+     
