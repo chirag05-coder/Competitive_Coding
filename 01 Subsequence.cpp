fcddb2b8793abf30f98b432a7ed645126ababf28
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-10-28 23:56:19 +0530

    Codechef

--- 01 Subsequence.cpp
+++ 01 Subsequence.cpp
@@ -60,22 +60,7 @@ bool is_prime(int num)
 		   }
 		return true;       
 }
-bool isPerfectSquare(long double x)
-{
-    // Find floating point value of
-    // square root of x.
-    if (x >= 0) {
- 
-        long long sr = sqrt(x);
-         
-        // if product of square root
-        //is equal, then
-        // return T/F
-        return (sr * sr == x);
-    }
-    // else return false if n<0
-    return false;
-}
+
 
 
 int main()
