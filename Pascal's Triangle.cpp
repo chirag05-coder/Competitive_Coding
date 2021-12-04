d1f35a53eabefe6c161b46af5de6e469bb40a2e0
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-12-04 14:34:42 +0530

    Leetcode

---
+++ Pascal's Triangle.cpp
@@ -0,0 +1,47 @@
+Link for Ques:
+https://leetcode.com/problems/pascals-triangle/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        
+        int dp[n][n];
+        for(int i=0;i<n;i++)
+        {    dp[i][0]=1;
+             for(int j=1;j<n;j++)
+             {
+                 if(i==j)
+                     dp[i][j]=1;
+                 else
+                     dp[i][j]=0;
+             }
+        }
+        
+    for(int i=2;i<n;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            if(dp[i][j]==1)
+                  break;
+            else
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+        }
+    }
+        
+    vector<vector<int>>v;
+         for(int i=0;i<n;i++)
+    {   vector<int>temp;
+          
+        for(int j=0;j<n;j++)
+        {
+            if(dp[i][j]==0)
+                  break;
+             temp.push_back(dp[i][j]);
+        }
+     v.push_back(temp);
+    }
+        return v;
+    }
+};
