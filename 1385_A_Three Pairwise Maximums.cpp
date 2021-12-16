9b922f5b3153d36511a15ab54d864a0e8b5dca3a
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-12-16 23:09:56 +0530

    Codeforces

---
+++ 1385_A_Three Pairwise Maximums.cpp
@@ -0,0 +1,81 @@
+Link for Ques:
+https://codeforces.com/contest/1385/problem/A
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+#define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+
+
+
+void solve() {
+
+	std::vector<int> v(3, 0);
+
+	ain(v)
+	sort(helper(v));
+
+	if (v[1] != v[2])
+		cout << "NO\n";
+	else {
+		cout << "YES\n";
+		cout << v[0] << " " << v[0] << " " << v[2] << "\n";
+
+	}
+
+
+}
+
+
+signed main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}
