065c218870a0821f6a76a409e9e2adde26dabc3f
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-12-18 01:44:07 +0530

    Codeforces

--- 1617_C_Paprika and Permutation.cpp
+++ 1617_C_Paprika and Permutation.cpp
@@ -8,17 +8,7 @@ https://codeforces.com/contest/1617/problem/C
 #define int long long int
 using namespace std;
 const int M=1e9+7;
-// lli power(lli a,lli b)
-// {
-//      if(b==0 )
-//         return 1;
-//       if(b==1)
-//       return a;
-     
-//       if(!(b&1)) 
-//       return (power(a,b/2)%M)*(power(a,b/2)%M);
-//       else
-//       return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
 
 // }
 
