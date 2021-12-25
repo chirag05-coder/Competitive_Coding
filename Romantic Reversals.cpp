94ec648ab7ec2f04dadfb7c5564ee715bc96c804
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2021-12-25 23:43:56 +0530

    Codechef

---
+++ Romantic Reversals.cpp
@@ -0,0 +1,123 @@
+Link for Ques:
+https://www.codechef.com/LTIME103B/problems/RMNTREV
+
+
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long int
+using namespace std;
+const int M=1e9+7;
+// lli power(lli a,lli b)
+// {
+//      if(b==0 )
+//         return 1;
+//       if(b==1)
+//       return a;
+     
+//       if(!(b&1)) 
+//       return (power(a,b/2)%M)*(power(a,b/2)%M);
+//       else
+//       return ((a)*(power(a,b/2)%M)%M)*(power(a,b/2)%M);
+
+// }
+
+// lli  gcd(lli u, lli v)
+// {
+//   lli shift;
+//   if (u == 0) return v;
+//   if (v == 0) return u;
+//   shift = __builtin_ctz(u | v);
+//   u >>= __builtin_ctz(u);
+//   do {
+//       v >>= __builtin_ctz(v);
+//       if (u > v) {
+//          lli t = v;
+//           v = u;
+//           u = t;
+//       }  
+//       v = v - u;
+//   } while (v != 0);
+//   return u << shift;
+// }
+// lli highestPowerof2(lli n)
+// {
+//   lli p = (int)log2(n);
+//   // return (int)pow(2, p);   power
+  
+//      return p;  //number
+// }
+// bool is_prime(int num)
+// {      if(num==1)
+//       return false;
+//       if(num==2 || num==3)
+// 	      return true;
+// 		if(num%2==0 || num%3==0)
+// 		     return false;
+// 	 for(int i=5;i*i<=num;i=i+6)
+// 	   {      if(num%i==0 || num%(i+2)==0)
+// 	              return false;
+// 		   }
+// 		return true;       
+// }
+// bool isPerfectSquare(long double x)
+// {
+//     // Find floating point value of
+//     // square root of x.
+//     if (x >= 0) {
+ 
+//         long long sr = sqrt(x);
+         
+//         // if product of square root
+//         //is equal, then
+//         // return T/F
+//         return (sr * sr == x);
+//     }
+//     // else return false if n<0
+//     return false;
+// }
+
+
+signed main()
+{    fastio;
+      int t;
+     cin>>t;
+    while(t--){
+         
+           int n,k;
+           cin>>n>>k;
+           string str;
+           cin>>str;
+           
+           vector<char>v(n,'$');
+           int i=0;
+             while(k>0)
+             {
+                 v[k-1]=str.at(i);
+                 k-=2;
+                 i++;
+             }
+               vector<int>v1;
+               for(int i=0;i<n;i++ )
+                {
+                    if(v[i]=='$')
+                       v1.push_back(i);
+                }
+                int j=0;
+                for(;i<n;i++)
+                 {
+                     v[v1[j]]=str.at(i);
+                     j++;
+                 }
+                 for(int i=0;i<n;i++)
+                    cout<<v[i];
+             cout<<endl;
+           
+            }
+           
+    }
+
+
+
+
+     
