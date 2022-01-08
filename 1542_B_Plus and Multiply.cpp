8f8415ad1d9369d6e5e717e1c8148d0875fdc529
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2022-01-08 22:48:29 +0530

    Create 1542_B_Plus and Multiply.cpp

---
+++ 1542_B_Plus and Multiply.cpp
@@ -0,0 +1,95 @@
+Link for Ques:
+https://codeforces.com/problemset/problem/1542/B
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll long long int
+#define int long long int
+#define pb push_back
+#define xxx exit(0)
+#define ain(arr) for (auto &it : arr) cin >> it;
+#define op(container) for(auto it: container)  cout<<it<<" ";
+#define clr  cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define sortv(arr) sort(arr.begin(),arr.end())
+#define sorta(arr,n) sort(arr,arr+n)
+#define helper(arr) arr.begin(),arr.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+const int inf = 1e9 + 7;
+
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+
+void solve() {
+
+	int n, a, b;
+	cin >> n >> a >> b;
+
+	if (a == 1) {
+		if ((n - 1) % b == 0) {
+			cout << "YES\n";
+			return;
+		}
+		else
+			cout << "NO\n";
+
+	}
+	else {
+		int x = 1;
+		while (x <= n) {
+			if ((n - x) % b == 0) {
+				cout << "YES\n";
+				return;
+			}
+			x *= a;
+		}
+		cout << "NO\n";
+
+	}
+
+
+
+
+}
+
+
+
+signed main(int argc, char const * argv[])
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << "\n";
+
+	return 0;
+
+}
+
+
+ 
