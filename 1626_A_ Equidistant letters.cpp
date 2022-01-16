88583d1be4a3a555cb45ff98319cdf71a0569390
chirag05-coder <57553671+chirag05-coder@users.noreply.github.com>
2022-01-16 23:44:34 +0530

    Codeforces

---
+++ 1626_A_ Equidistant letters.cpp
@@ -0,0 +1,25 @@
+Link for Ques:
+https://codeforces.com/contest/1626/problem/A
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    
+    while(t--)
+    {
+       string str;
+       cin>>str;
+       sort(str.begin(),str.end());
+       cout<<str<<endl;
+        
+    }
+    
+   
+}
